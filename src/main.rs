ce529ae072f7923abd45839d60fcedd692f2f480
Peter Kaye <peter.kaye@sky.uk>
2023-11-06 15:50:42 +0000

    implemented multi-threading

--- src/main.rs
+++ src/main.rs
@@ -21,7 +21,7 @@ fn pixel_to_point(bounds: (usize,usize),pixel:(usize,usize),upper_left:Complex<f
         let (width,height) = (lower_right.re - upper_left.re,upper_left.im-lower_right.im);
         Complex{
             re:upper_left.re + pixel.0 as f64 * width / bounds.0 as f64,
-            im:upper_left.im - pixel.1 as f64 * width / bounds.1 as f64
+            im:upper_left.im - pixel.1 as f64 * height / bounds.1 as f64
         }
 }
 
@@ -33,17 +33,7 @@ fn render(pixels: &mut[u8],bounds:(usize,usize),upper_left:Complex<f64>,lower_ri
             pixels[row * bounds.0 + column] = match escape_time(point,255){
                 None=>0,
                 Some(count) =>255-count as u8
-            };fn escape_time(c: Complex<f64>, limit: usize) -> Option<usize> {
-                let mut z = Complex { re: 0.0, im: 0.0 };
-                for i in 0..limit {
-                    if z.norm_sqr() > 4.0 {
-                        return Some(i);
-                    }
-                    z = z * z + c;
-                }
-            
-                None
-            }
+            };
         }
     }
 }
@@ -55,16 +45,6 @@ fn parse_complex (s: &str)->Option<Complex<f64>> {
     }
 }
 
-fn complex_square_add_loop(c:Complex<f64>, limit:usize)->Option<usize>{
-    let mut z = Complex {re:0.0,im:0.0};
-    for i in 0..limit{
-        if z.norm_sqr() > 4.0{
-            return Some(i);
-        }
-        z=z*z+c;
-    }
-    None
-}
 
 fn write_image(filename: &str,pixels: &[u8],bounds: (usize,usize))-> Result<(),std::io::Error>{
     let output = File::create(filename)?;
@@ -107,7 +87,25 @@ fn main() {
 
     let mut pixels = vec![0; bounds.0 * bounds.1];
 
-    render(&mut pixels, bounds, upper_left, lower_right);
+    let threads = 8;
+    let rows_per_band = bounds.1 / threads + 1;
+
+    
+    let bands: Vec<&mut [u8]> = pixels.chunks_mut(rows_per_band * bounds.0).collect();
+    crossbeam::scope(|spawner| {
+        for (i, band) in bands.into_iter().enumerate() {
+            let top = rows_per_band * i;
+            let height = band.len() / bounds.0;
+            let band_bounds = (bounds.0, height);
+            let band_upper_left = pixel_to_point(bounds, (0, top), upper_left, lower_right);
+            let band_lower_right = pixel_to_point(bounds, (bounds.0, top + height), upper_left, lower_right);
+
+            spawner.spawn(move |_| {
+                render(band, band_bounds, band_upper_left, band_lower_right);
+            });
+        }
+    }).unwrap();
+
 
     write_image(&args[1], &pixels, bounds)
         .expect("error writing PNG file");
